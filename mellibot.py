8df8b901de85aa9c4c8a121ffe47795665c070df
rahi20 <largorahima@gmail.com>
2021-06-15 18:21:58 +0100

    actions

--- mellibot.py
+++ mellibot.py
@@ -9,12 +9,13 @@ import preprocess as pr
 import numpy as np
 import json
 import os
+import actions.actions as ac
 
 model = load_model(os.path.join('saved_models_vars', 'bot_hypermodel.h5'))
 classes = pickle.load(open(os.path.join('saved_models_vars','val_label_cor.pckl'), 'rb'))
 data_file = open(os.path.join("data","intents.json"))
 intents = json.loads(data_file.read())
-
+rules = json.loads(open(os.path.join('data','rules.json')).read())
 
 def predict_intent(msg, model):
     treshold = 0.25
@@ -34,59 +35,27 @@ def predict_intent(msg, model):
             #intent_label = classes.iloc[np.argmax(pp[0])]['intent_label']
     return return_intents
 
-def getResponse(ints, intents_json):
+def getResponse(ints, intents_json, msg):
     tag = ints[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
         if(i['tag'] == tag):
-            result = random.choice(i['responses'])
-            break
-            
+            #intents_file = open(os.path.join("data","intents.json"))
+            #data = json.load(intents_file)
+            method_name = rules.get(tag, None)
+            if method_name is not None: 
+                action_method = getattr(ac, method_name)
+                result = action_method(msg)
+            else :
+                result = random.choice(i['responses'])
     return result
 
 def chatbot_response(msg):
     ints = predict_intent(msg, model)
-    res = getResponse(ints, intents)
+    res = getResponse(ints, intents, msg)
     return res
 
-"""
-class BotRes:
-    def __init__(self) -> None:
-        pass
-    def predict_intent(self, msg, model):
-        treshold = 0.25
-        dd = np.array([pr.wordvec(msg)])
-        pp = model.predict(dd)
 
-        it = np.nditer(pp, flags=['c_index'])
-
-        results = [[it.index,float(x)] for x in it if x>0.25]
-
-        results = sorted(results, key=lambda x: x[1], reverse=True)
-
-        return_intents = []
-        for r in results:
-            return_intents.append({"intent": classes.iloc[r[0]]['intent_label'], "probability": str(r[1])})
-
-            #intent_label = classes.iloc[np.argmax(pp[0])]['intent_label']
-        return return_intents
-
-    def getResponse(self, ints, intents_json):
-        tag = ints[0]['intent']
-        list_of_intents = intents_json['intents']
-        for i in list_of_intents:
-            if(i['tag'] == tag):
-                result = random.choice(i['responses'])
-                break
-            
-        return result
-
-    def chatbot_response(self, msg):
-        ints = self.predict_intent(msg, model)
-        res = self.getResponse(ints, intents)
-        return res
-
-"""
 def send( msg):
     ChatLog.config(state = DISABLED)
     msg = msg
@@ -163,84 +132,3 @@ SendButton.place(relx = 0.77,
 window.mainloop()
     
  
-"""
-class Gui:
-    def __init__(self):
-        self.window = Tk()
-        self.window.title("Chat with MelliBot")
-        self.window.resizable(width = False, height = False)
-        self.window.configure(width = 470, height = 550, bg = "#e3e3e3")
-
-        self.ChatLog = Text(self.window,
-                            width = 20, 
-                             height = 2,
-                             bg = "#E3E3E3",
-                             fg = "#010101",
-                             font = "Verdana 13", 
-                             padx = 5,
-                             pady = 5)
-        self.ChatLog.config(state=DISABLED)
-        
-        self.scrollbar = Scrollbar(self.window, command=self.ChatLog.yview)
-        self.ChatLog['yscrollcommand'] = self.scrollbar.set
-
-        self.labelBottom = Label(self.window,
-                                 bg = "#C4C7CC",
-                                 height = 80)
-          
-        self.labelBottom.place(relwidth = 1,
-                               rely = 0.825)
-        
-        self.EntryBox = Entry(self.labelBottom,
-                              bg = "#E3E3E3",
-                              fg = "#010101",
-                              font="Verdana 13")
-        
-        self.EntryBox.place(relwidth = 0.74,
-                            relheight = 0.09,
-                            rely = 0.008,
-                            relx = 0.011)
-
-        self.SendButton = Button(
-            self.labelBottom, 
-            font=("Verdana",12,'bold'), 
-            text="Send", 
-            width=20, 
-            bg="#43655A", 
-            activebackground="#5D7D7C",
-            fg='#E3E3E3',
-            command= lambda : self.send(self.EntryBox.get()))
-        #self.EntryBox = Text(self.window, bd=0, bg="white",width="29", height="5", font="Arial")
-
-        self.scrollbar.place(relheight = 1, relx = 0.974)
-        self.ChatLog.place(relheight = 0.825,
-                            relwidth = 1, 
-                            rely = 0.0)
-
-        self.SendButton.place(relx = 0.77,
-                             rely = 0.014,
-                             relheight = 0.075, 
-                             relwidth = 0.22)
-
-        self.window.mainloop()
-    
-    def send(self, msg):
-        self.ChatLog.config(state = DISABLED)
-        self.msg = msg
-        self.EntryBox.delete(0, END)
-        
-        if msg != '':
-            self.ChatLog.config(state=NORMAL)
-            self.ChatLog.insert(END, "You : " + msg + '\n')
-            self.ChatLog.config(foreground="#010101", font=("Verdana", 12, font.BOLD))
-            
-            res = BotRes.chatbot_response(msg)
-            self.ChatLog.insert(END, "MelliBot : " + res + '\n')
-            
-            self.ChatLog.config(state=DISABLED)
-            self.ChatLog.yview(END)
-            
-            
-
-gui = Gui()
-"""
